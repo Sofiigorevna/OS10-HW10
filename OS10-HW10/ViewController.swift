eb62b7abfcb6710ef2a4a60a88826ea3a063a7ed
sofiigorevna <sofia.konchencko@yandex.ru>
2023-05-27 13:20:41 +0300

    Initial Commit

---
+++ OS10-HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OS10-HW10
+//
+//  Created by 1234 on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
